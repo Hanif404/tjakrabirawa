615aff8ee3437a915700c4cb29a90449f9e48e7f
Hanif <hanif.kharismadini.04@gmail.com>
2017-05-31 11:38:12 +0700

    mengubah css blog page reponsif

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '7Bionic.';
+$db['default']['password'] = 'wrc123';
 $db['default']['database'] = 'layout4db';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
